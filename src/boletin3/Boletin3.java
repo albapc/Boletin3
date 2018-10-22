04d22fb9d03d8d2aca69a0062032a243d466c2e9
aperezcesar <aperezcesar@nautilus1.danielcastelao.org>
2018-10-22 11:53:46 +0200

    correccion

--- src/boletin3/Boletin3.java
+++ src/boletin3/Boletin3.java
@@ -8,13 +8,15 @@ public class Boletin3 {
     public static void main(String[] args) {
        
        Consumo obxConsume= new Consumo();
-       /*float litros=50f;
-       float pGas= 1.57f;*/
+       /*obxConsume.setLitros(50f);
+       obxConsume.setpGas(1.57f);*/
        
        Consumo obxConsumo= new Consumo(500f, 40f, 28f, 1.57f);
+       obxConsumo.setLitros(15f);
        obxConsumo.consumoMedio();
        obxConsumo.setvMed(60);
        System.out.println("Velocidad media: "+obxConsumo.getvMed()+" km/h");
+       
     }
     
 }
